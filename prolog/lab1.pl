040eb2d2ff36760546df52437fcd22dd9198ed6d
mallekbh <mallek.bh@gmail.com>
2018-10-23 10:26:32 +0100

    update(ri): lab1 - familly members

--- prolog/lab1.pl
+++ prolog/lab1.pl
@@ -9,7 +9,7 @@ appartient(X,[_|L]):-appartient(X,L).
 
 %% premier(X,[X,_]).
 %% dernier(X,[X]):-!.
-%% dernier(X,[_,Y|L]):-dernier(X,[Y,L]).  %testing premier(X,[1,2]). dernier(X,[1,2,3,4]).
+dernier(X,[_,Y|L]):-dernier(X,[Y,L]).  %testing premier(X,[1,2]). dernier(X,[1,2,3,4]).
 
 %% 3/
 
@@ -35,12 +35,108 @@ longeur(K,[_,Y|L]):-
 longeur(K2,[Y|L]),
 K2 is K-1.
 
+%% concatenation de deux listes
+concat([],L,L).
+concat([X|L1],L2,[X|L3]):-
+concat(L1,L2,L3).
 
+%% test palindrome
+palindrome([]).
+palindrome([_]).
+palindorme([X,Y|L]):-
+dernier(Z,[Y|L]),
+X==Z,
+longeur([Y|L],K),
+supprimer_k(K,[Y|L],L2),
+palindrome(L2).
 
+%% exo2
 
+homme(ali).
+homme(hacene).
+homme(hakim).
+homme(mohamed).
+homme(said).
+homme(samir).
 
+femme(djamila).
+femme(fatma).
+femme(houria).
+femme(linda).
+femme(lilia).
 
+pere(mohamed,samir).
+pere(samir,lilia).
+pere(samir,said).
+pere(said,hacene).
+pere(said,linda).
+pere(hakim,ali).
 
+mere(fatma,samir).
+mere(houria,lilia).
+mere(houria,said).
+mere(lilia,ali).
+mere(djamila,hacene).
+mere(djamila,linda).
+
+parent(X,Y):-
+pere(X,Y).
+parent(X,Y):-
+mere(X,Y).
+
+fils(X,Y):-
+homme(Y),
+pere(Y,X).
+
+fils(X,Y):-
+homme(X),
+mere(Y,X).
+
+fille(X,Y):-
+femme(Y),
+pere(Y,X).
+
+fille(X,Y):-
+femme(X),
+mere(Y,X).
+
+grand_pere(X,Y):-
+pere(X,Z),
+parent(Z,Y).
+
+grand_mere(X,Y):-
+mere(X,Z),
+parent(Z,Y).
+
+frere(X,Y):-
+homme(X),
+pere(Z,X),
+pere(Z,Y).
+
+soeur(X,Y):-
+femme(X),
+pere(Z,X),
+pere(Z,Y).
+
+oncle(X,Y):-
+frere(X,Z),
+parent(Z,Y).
+
+tonte(X,Y):-
+soeur(X,Z),
+parent(Z,Y).
+
+%% es ce que x est homme ?
+homme(said).
+homme(lilia).
+
+%% es ce que x est femme ?
+femme(lilia).
+femme(houria).
+
+%% qui sont les fils de said ?
+pere(said,X).
+pere(houria,X).  
 
 
 
